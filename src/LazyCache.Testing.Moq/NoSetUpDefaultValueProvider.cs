cbb0017443d4d34480563385b3e1075cb44cb281
Lee Anderson <arrgeeveelee@gmail.com>
2019-08-04 10:18:34 +0800

    Updated the no set up default value provider/no set up handler to set up the a mock match where possible. NSubstitute allows us to set up a cache entry for the add and get or add invocations. Moq4 only allows for the get or add invocation.

--- src/LazyCache.Testing.Moq/NoSetUpDefaultValueProvider.cs
+++ src/LazyCache.Testing.Moq/NoSetUpDefaultValueProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using LazyCache.Testing.Extensions;
-using Microsoft.Extensions.Caching.Memory;
 using Moq;
 
 namespace LazyCache.Testing.Moq {
@@ -9,25 +8,60 @@ namespace LazyCache.Testing.Moq {
     /// Default value provider for methods that have not been set up on a lazy cache mock.
     /// </summary>
     public class NoSetUpDefaultValueProvider : DefaultValueProvider {
+        private readonly Mock<IAppCache> _cachingServiceMock;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cachingServiceMock">An instance of the caching service mock.</param>
+        public NoSetUpDefaultValueProvider(Mock<IAppCache> cachingServiceMock) {
+            _cachingServiceMock = cachingServiceMock;
+        }
+
         /// <summary>
         ///     Checks the current method invocation on the mock;
-        ///     if GetOrAdd or GetOrAddAsync was invoked the addItemFactory result will be returned;
+        ///     if GetOrAdd or GetOrAddAsync was invoked the unexpected match is set up and the addItemFactory result will be returned;
         ///     otherwise the default value for the specified type will be returned.
         /// </summary>
         /// <param name="type">The type of the requested default value.</param>
         /// <param name="mock">The mock on which an unexpected invocation has occurred.</param>
         /// <returns>
-        ///     if GetOrAdd or GetOrAddAsync was invoked the addItemFactory result will be returned;
+        ///     if GetOrAdd or GetOrAddAsync was invoked the unexpected match is set up and the addItemFactory result will be returned;
         ///     otherwise the default value for the specified type will be returned.
         /// </returns>
         protected override object GetDefaultValue(Type type, Mock mock) {
+            Console.WriteLine("NoSetUpDefaultValueProvider invoked");
+
             var lastInvocation = mock.Invocations.Last();
-            if (!lastInvocation.Method.Name.StartsWith("GetOrAdd", StringComparison.CurrentCultureIgnoreCase))
-                return type.GetDefaultValue();
+            var methodInfo = lastInvocation.Method;
+
+            var args = lastInvocation.Arguments;
             
-            var func = (Func<ICacheEntry, object>)lastInvocation.Arguments[1];
-            var result = func(new CacheEntryFake());
-            return result;
+            if (methodInfo.Name.StartsWith("GetOrAdd", StringComparison.CurrentCultureIgnoreCase)) {
+                //We have everything we need to set up a match, so let's do it
+                var key = args[0].ToString();
+
+                var funcType = methodInfo.GetParameters()[1].ParameterType;
+                Console.WriteLine($"{funcType} methods: ");
+                foreach (var mi in funcType.GetMethods()) {
+                    Console.WriteLine(mi);
+                }
+
+                var item = funcType.GetMethod("Invoke").Invoke(args[1], new object[] { new CacheEntryFake(key) });
+
+                var itemType = item.GetType();
+
+                var method = typeof(LazyCache.Testing.Moq.Extensions.MockExtensions).GetMethods().Single(mi => mi.Name.Equals("SetUpCacheEntry"));
+                method.MakeGenericMethod(itemType).Invoke(null, new object[] { _cachingServiceMock, key, item });
+
+                return item;
+            }
+            
+            if (methodInfo.ReturnType == typeof(void)) {
+                return null;
+            }
+
+            return methodInfo.ReturnType.GetDefaultValue();
         }
     }
 }
--- src/LazyCache.Testing.NSubstitute/NoSetUpHandler.cs
+++ src/LazyCache.Testing.NSubstitute/NoSetUpHandler.cs
@@ -1,33 +1,80 @@
 ﻿using System;
 using System.Linq;
 using LazyCache.Testing.Extensions;
-using Microsoft.Extensions.Caching.Memory;
+using LazyCache.Testing.NSubstitute.Extensions;
 using NSubstitute.Core;
 
 namespace LazyCache.Testing.NSubstitute {
     /// <summary>
     /// Handler for methods that have not been set up on a lazy cache mock.
     /// </summary>
-    public class NoSetUpHandler : ICallHandler{
+    public class NoSetUpHandler : ICallHandler {
+        private readonly IAppCache _cachingServiceMock;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cachingServiceMock">An instance of the caching service mock.</param>
+        public NoSetUpHandler(IAppCache cachingServiceMock) {
+            _cachingServiceMock = cachingServiceMock;
+        }
+
         /// <summary>
         ///     Checks the current method invocation on the mock;
-        ///     if GetOrAdd or GetOrAddAsync was invoked the addItemFactory result will be returned;
+        ///     if Add was invoked the unexpected match is set up;
+        ///     if GetOrAdd or GetOrAddAsync was invoked the unexpected match is set up and the addItemFactory result will be returned;
         ///     otherwise the default value for the specified type will be returned.
         /// </summary>
         /// <param name="call"></param>
         /// <returns>
-        ///     if GetOrAdd or GetOrAddAsync was invoked the addItemFactory result will be returned;
+        ///     if Add was invoked the unexpected match is set up;
+        ///     if GetOrAdd or GetOrAddAsync was invoked the unexpected match is set up and the addItemFactory result will be returned;
         ///     otherwise the default value for the specified type will be returned.
         /// </returns>
         public RouteAction Handle(ICall call) {
-            var methodInfo = call.GetMethodInfo();
-            
-            if (!methodInfo.Name.StartsWith("GetOrAdd", StringComparison.CurrentCultureIgnoreCase))
-                return RouteAction.Return(methodInfo.ReturnType.GetDefaultValue());
+            Console.WriteLine("NoSetUpHandler invoked");
 
-            var func = (Func<ICacheEntry, object>)call.GetArguments().ElementAt(1);
-            var result = func(new CacheEntryFake());
-            return RouteAction.Return(result);
+            var methodInfo = call.GetMethodInfo();
+            var args = call.GetArguments();
+            
+            if (methodInfo.Name.StartsWith("Add", StringComparison.CurrentCultureIgnoreCase)){
+                //We have everything we need to set up a match, so let's do it
+                var key = args[0].ToString();
+                var item = args[1];
+                var itemType = methodInfo.GetParameters()[1].ParameterType;
+
+                var method = typeof(MockExtensions).GetMethods().Single(mi => mi.Name.Equals("SetUpCacheEntry"));
+                method.MakeGenericMethod(itemType).Invoke(null, new object[] { _cachingServiceMock, key, item });
+
+                return RouteAction.Return(null);
+            }
+
+            if(methodInfo.Name.StartsWith("GetOrAdd", StringComparison.CurrentCultureIgnoreCase))
+            {
+                //We have everything we need to set up a match, so let's do it
+                var key = args[0].ToString();
+
+                var funcType = methodInfo.GetParameters()[1].ParameterType;
+                Console.WriteLine($"{funcType} methods: ");
+                foreach (var mi in funcType.GetMethods()) {
+                    Console.WriteLine(mi);
+                }
+
+                var item = funcType.GetMethod("Invoke").Invoke(args[1], new []{ new CacheEntryFake(key) });
+
+                var itemType = item.GetType();
+
+                var method = typeof(MockExtensions).GetMethods().Single(mi => mi.Name.Equals("SetUpCacheEntry"));
+                method.MakeGenericMethod(itemType).Invoke(null, new object[] { _cachingServiceMock, key, item });
+                
+                return RouteAction.Return(item);
+            }
+
+            if (methodInfo.ReturnType == typeof(void)) {
+                return RouteAction.Return(null);
+            }
+
+            return RouteAction.Return(methodInfo.ReturnType.GetDefaultValue());
         }
     }
 }
