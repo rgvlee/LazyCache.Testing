5a04b6cdfc207e3c7979edab5180b4fe2f7db5c8
Lee Anderson <arrgeeveelee@gmail.com>
2019-08-04 10:09:28 +0800

    Updated the test object to include an Id property for more vigour in the tests

--- src/LazyCache.Testing.Moq.Tests/TestObject.cs
+++ src/LazyCache.Testing.Moq.Tests/TestObject.cs
@@ -1,5 +1,7 @@
+using System;
+
 namespace LazyCache.Testing.Moq.Tests {
     public class TestObject {
-
+        public Guid Id { get; set; } = Guid.NewGuid();
     }
 }
--- src/LazyCache.Testing.NSubstitute.Tests/TestObject.cs
+++ src/LazyCache.Testing.NSubstitute.Tests/TestObject.cs
@@ -1,5 +1,7 @@
+using System;
+
 namespace LazyCache.Testing.NSubstitute.Tests {
     public class TestObject {
-
+        public Guid Id { get; set; } = Guid.NewGuid();
     }
 }
