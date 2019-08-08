59f58c583babd5b91aa68d7911b3955a102c268f
Lee Anderson <arrgeeveelee@gmail.com>
2019-08-08 20:37:26 +0800

    Refactored tests to use test base

--- src/LazyCache.Testing.Moq.Tests/Tests.cs
+++ src/LazyCache.Testing.Moq.Tests/Tests.cs
@@ -1,71 +1,31 @@
-using LazyCache.Testing.Helpers;
+using LazyCache.Testing.Common.Tests;
 using LazyCache.Testing.Moq.Extensions;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using System;
-using System.Threading.Tasks;
 
 namespace LazyCache.Testing.Moq.Tests {
     [TestFixture]
-    public class Tests {
-        private static readonly ILogger<Tests> Logger = LoggerHelper.CreateLogger<Tests>();
+    public class Tests : LazyCache.Testing.Common.Tests.Tests {
+        protected Mock<IAppCache> cacheMock;
+        
+        protected override void SetUpCacheEntry<T>(string cacheEntryKey, T expectedResult) {
+            cacheMock.SetUpCacheEntry(cacheEntryKey, expectedResult);
+        }
 
         [SetUp]
-        public void SetUp() {
-            var loggerFactory = new LoggerFactory();
-            loggerFactory.AddConsole(LogLevel.Debug);
-            LoggerHelper.LoggerFactory = loggerFactory;
+        public override void SetUp() {
+            base.SetUp();
+
+            cacheMock = MockFactory.CreateCachingServiceMock();
+            mockedCache = cacheMock.Object;
         }
-
-        [Test]
-        public void GetOrAddWithSetUp_Guid_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            cacheMock.SetUpCacheEntry(cacheEntryKey, expectedResult);
-            var mockedCache = cacheMock.Object;
-
-            var actualResult = mockedCache.GetOrAdd(cacheEntryKey, () => expectedResult, DateTimeOffset.Now.AddMinutes(30));
-            
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public async Task GetOrAddAsyncWithSetUp_Guid_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            cacheMock.SetUpCacheEntry(cacheEntryKey, expectedResult);
-            var mockedCache = cacheMock.Object;
-
-            var actualResult = await mockedCache.GetOrAddAsync(cacheEntryKey, () => Task.FromResult(expectedResult));
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public void GetWithNoSetUp_ReturnsDefaultValue() {
-            var cacheEntryKey = "SomethingInTheCache";
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
-            var actualResult = mockedCache.Get<Guid>(cacheEntryKey);
-
-            Assert.That(actualResult, Is.EqualTo(default(Guid)));
-        }
-
+        
         [Test]
         public void GetOrAddWithNoSetUp_Guid_ReturnsExpectedResult() {
             var cacheEntryKey = "SomethingInTheCache";
             var expectedResult = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
 
             var actualResult = mockedCache.GetOrAdd(cacheEntryKey, () => expectedResult, DateTimeOffset.Now.AddMinutes(30));
 
@@ -79,10 +39,7 @@ namespace LazyCache.Testing.Moq.Tests {
         public void GetOrAddWithNoSetUp_TestObject_ReturnsExpectedResult() {
             var cacheEntryKey = "SomethingInTheCache";
             var expectedResult = new TestObject();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
+            
             var actualResult = mockedCache.GetOrAdd(cacheEntryKey, () => expectedResult, DateTimeOffset.Now.AddMinutes(30));
 
             Assert.Multiple(() => {
@@ -92,41 +49,12 @@ namespace LazyCache.Testing.Moq.Tests {
         }
 
         [Test]
-        public async Task GetOrAddAsyncWithNoSetUp_Guid_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
-            var actualResult = await mockedCache.GetOrAddAsync(cacheEntryKey, () => Task.FromResult(expectedResult));
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public async Task GetOrAddAsyncWithNoSetUp_TestObject_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = new TestObject();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
-            var actualResult = await mockedCache.GetOrAddAsync(cacheEntryKey, () => Task.FromResult(expectedResult));
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
         public void GetThenGetOrAddThenGetWithNoSetUp_TestObject_ReturnsExpectedResults() {
             var cacheEntryKey = "SomethingInTheCache";
             var expectedResult1 = default(TestObject);
             var expectedResult2 = new TestObject();
             var expectedResult3 = expectedResult2;
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
+            
             var actualResult1 = mockedCache.Get<TestObject>(cacheEntryKey);
             var actualResult2 = mockedCache.GetOrAdd(cacheEntryKey, () => expectedResult2, DateTimeOffset.Now.AddMinutes(30));
             var actualResult3 = mockedCache.Get<TestObject>(cacheEntryKey);
@@ -150,9 +78,7 @@ namespace LazyCache.Testing.Moq.Tests {
             var cacheEntryKey = "SomethingInTheCache";
             var expectedResult = new TestObject();
 
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            cacheMock.SetUpCacheEntry(cacheEntryKey, expectedResult);
-            var mockedCache = cacheMock.Object;
+            SetUpCacheEntry(cacheEntryKey, expectedResult);
             
             var actualResult1 = mockedCache.Get<TestObject>(cacheEntryKey);
             var actualResult2 = mockedCache.GetOrAdd(cacheEntryKey, addItemFactory: () => expectedResult, expires: DateTimeOffset.Now.AddMinutes(30));
@@ -165,106 +91,6 @@ namespace LazyCache.Testing.Moq.Tests {
 
                 cacheMock.Verify(m => m.GetOrAdd(It.IsAny<string>(), It.IsAny<Func<ICacheEntry, TestObject>>()), Times.Once);
                 cacheMock.Verify(m => m.Get<TestObject>(It.IsAny<string>()), Times.Exactly(2));
-            });
-        }
-
-        [Test]
-        public void MinimumViableInterface_Guid_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = Guid.NewGuid();
-
-            var mockedCache = MockFactory.CreateMockedCachingService();
-            
-            var actualResult = mockedCache.GetOrAdd(cacheEntryKey, () => expectedResult, DateTimeOffset.Now.AddMinutes(30));
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        //The ability to handle an unexpected void method is not supported by Moq4
-        //so I am using a dynamic proxy on the mock to ensure that add methods are set up automatically
-        [Test]
-        public void AddThenGetWithNoSetUp_Guid_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
-            Logger.LogDebug("Add invocation started");
-            mockedCache.Add(cacheEntryKey, expectedResult);
-            Logger.LogDebug("Add invocation finished");
-            var actualResult = mockedCache.Get<Guid>(cacheEntryKey);
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public void AddThenGetWithNoSetUp_TestObject_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult = new TestObject();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
-            mockedCache.Add(cacheEntryKey, expectedResult);
-
-            var actualResult = mockedCache.Get<TestObject>(cacheEntryKey);
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public void AddThenGetWithSetUp_Guid_ReturnsExpectedResult() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult1 = Guid.NewGuid();
-            var expectedResult2 = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            cacheMock.SetUpCacheEntry(cacheEntryKey, expectedResult1);
-            var mockedCache = cacheMock.Object;
-
-            var actualResult1 = mockedCache.Get<Guid>(cacheEntryKey);
-            Assert.AreEqual(expectedResult1, actualResult1);
-
-            mockedCache.Add(cacheEntryKey, expectedResult2);
-            var actualResult2 = mockedCache.Get<Guid>(cacheEntryKey);
-
-            Assert.AreEqual(expectedResult2, actualResult2);
-        }
-
-        [Test]
-        public void RemoveWithSetUp_Guid_ReturnsDefaultValue() {
-            var cacheEntryKey = "SomethingInTheCache";
-            var expectedResult1 = Guid.NewGuid();
-
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            cacheMock.SetUpCacheEntry(cacheEntryKey, expectedResult1);
-            var mockedCache = cacheMock.Object;
-
-            var actualResult1 = mockedCache.Get<Guid>(cacheEntryKey);
-            mockedCache.Remove(cacheEntryKey);
-            var actualResult2 = mockedCache.Get<Guid>(cacheEntryKey);
-
-            Assert.Multiple(() => {
-                Assert.AreEqual(expectedResult1, actualResult1);
-                Assert.That(actualResult2, Is.EqualTo(default(Guid)));
-            });
-        }
-
-        [Test]
-        public void RemoveWithNoSetUp_Guid_ReturnsDefaultValue() {
-            var cacheEntryKey = "SomethingInTheCache";
-            
-            var cacheMock = MockFactory.CreateCachingServiceMock();
-            var mockedCache = cacheMock.Object;
-
-            var actualResult1 = mockedCache.Get<Guid>(cacheEntryKey);
-            mockedCache.Remove(cacheEntryKey);
-            var actualResult2 = mockedCache.Get<Guid>(cacheEntryKey);
-
-            Assert.Multiple(() => {
-                Assert.That(actualResult1, Is.EqualTo(default(Guid)));
-                Assert.That(actualResult2, Is.EqualTo(default(Guid)));
             });
         }
     }
