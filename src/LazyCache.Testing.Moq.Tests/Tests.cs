d8419598197c80e47eb4b1de0761d24ecbf6f7e2
Lee Anderson <arrgeeveelee@gmail.com>
2019-10-12 12:23:57 +0800

    Added tests to ensure verify/received methods work as expected with the automatic set ups

--- src/LazyCache.Testing.Moq.Tests/Tests.cs
+++ src/LazyCache.Testing.Moq.Tests/Tests.cs
@@ -1,5 +1,7 @@
 using LazyCache.Testing.Common.Tests;
 using LazyCache.Testing.Moq.Extensions;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
 using NUnit.Framework;
 
 namespace LazyCache.Testing.Moq.Tests {
@@ -14,6 +16,27 @@ namespace LazyCache.Testing.Moq.Tests {
 
         protected override void SetUpCacheEntry<T>(string cacheEntryKey, T expectedResult) {
             MockedCache.SetUpCacheEntry(cacheEntryKey, expectedResult);
+        }
+
+        [Test]
+        public virtual void AddWithNoSetUp_TestObject_AddInvokedOnce() {
+            var cacheEntryKey = "SomethingInTheCache";
+            var expectedResult = new TestObject();
+
+            MockedCache.Add(cacheEntryKey, expectedResult);
+            
+            Mock.Get(MockedCache).Verify(m => m.Add(cacheEntryKey, expectedResult, It.IsAny<MemoryCacheEntryOptions>()), Times.Once);
+        }
+
+        [Test]
+        public virtual void AddThenGetWithNoSetUp_TestObject_GetInvokedOnce() {
+            var cacheEntryKey = "SomethingInTheCache";
+            var expectedResult = new TestObject();
+
+            MockedCache.Add(cacheEntryKey, expectedResult);
+            var actualResult = MockedCache.Get<TestObject>(cacheEntryKey);
+
+            Mock.Get(MockedCache).Verify(m => m.Get<TestObject>(cacheEntryKey), Times.Once);
         }
     }
 }
